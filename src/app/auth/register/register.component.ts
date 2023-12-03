a05a60e303b41085f950f931c0ed95e1d63fdc13
Elizabeth Zepeda <ke.zepeda@gmail.com>
2023-12-03 16:19:50 -0500

    ReactiveForms settup & creating authService

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/authService.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registroForm: FormGroup;
+
+  constructor( private fb: FormBuilder, private authService: AuthService) { }
+
 
   ngOnInit() {
+    this.registroForm = this.fb.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      contrasena: ['', Validators.required],
+    })
   }
 
+  crearUsuario() {
+
+    if ( !this.registroForm.valid ) return;
+
+    const { nombre, correo, contrasena } = this.registroForm.value;
+
+    this.authService.crearUsuario(nombre, correo, contrasena);
+
+
+  }
 }
