432c188ad7d6b5bdcea9f195216d6b08f387d2e2
Elizabeth Zepeda <ke.zepeda@gmail.com>
2023-12-03 18:04:43 -0500

    initAuthUser

--- src/app/services/authService.service.ts
+++ src/app/services/authService.service.ts
@@ -8,6 +8,14 @@ export class AuthService {
 
 constructor(private _angularFireAuth: AngularFireAuth) { }
 
+  initAuthUser() {
+    this._angularFireAuth.authState.subscribe(fuser =>{
+      console.log(fuser);
+      console.log(fuser?.email);
+      console.log(fuser?.uid);
+    })
+  }
+
   crearUsuario(nombre:string, email:string, password:string)  {
     return this._angularFireAuth.createUserWithEmailAndPassword(email, password);
     // console.log({nombre, email, password})
